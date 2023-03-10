8d63be30a69cab5ed7c2ad6c259217c79a51c9d8
eric <eric@antlabs.com>
2023-03-10 20:10:19 +0800

    Changed Exceptions folder to Exception

---
+++ src/Exception/CurlException.php
@@ -0,0 +1,7 @@
+<?php
+namespace Curly\Exception;
+
+class CurlException extends \Exception
+{
+    
+}
--- src/Exceptions/CurlException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Curly\Exceptions;
-
-class CurlException extends \Exception
-{
-    
-}
